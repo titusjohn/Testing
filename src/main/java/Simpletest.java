d1ecd01d6f790d49dfd91599eef821ebc748fabc
titusjohn <titusjohn@gmail.com>
2014-09-12 17:17:23 -0400

    commit

--- src/main/java/Simpletest.java
+++ src/main/java/Simpletest.java
@@ -1,3 +1,10 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Simpletest {
 
@@ -5,7 +12,19 @@ public class Simpletest {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Triggered the script");
-
-	}
+		
+		 WebDriver driver = new FirefoxDriver();
+	        
+	        // Go to the Google Suggest home page
+	        driver.get("http://www.google.com/webhp?complete=1&hl=en");
+	        
+	        // Enter the query string "Cheese"
+	        WebElement query = driver.findElement(By.name("q"));
+	        query.sendKeys("Cheese");
+	        
+	        query.submit();
+	
+	        //driver.quit();
+	    }	
 
 }
